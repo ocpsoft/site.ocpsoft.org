e077a577e8e80f9c0e0065e492e9d6560659536a
root <root@ocpsoft.org>
2013-04-10 09:41:30 -0400

    Update error page

--- error.php
+++ error.php
@@ -1,5 +1,5 @@
 <div class="row" style="text-align: center;">
-	<div class="span6 offset3 alert alert-error" style="margin-top: 30px;">
+	<div class="span6 offset3 alert alert-error" style="margin-top: 30px; position: fixed; opacity: .9; z-index: 10;">
 		<h1>Sorry, this page has been eaten by sharks</h1>
 		Maybe you could try another page? That one might still be around. If you want to look at some <a href="<?php bloginfo('url'); ?>">cool open-source software</a>, you could do
 		that, too; otherwise, if you're sticking around, could you feed our pets? They're not angry, they just get <i>so hungry.</i>
@@ -7,7 +7,7 @@
 </div>
 <div class="row" style="text-align: center;">
 	<div>
-		<a href="javascript:alert('Nom nom nom!');"> <img style="width: 85%; cursor: url();" src="<?php bloginfo('stylesheet_directory');?>/img/shark.png" width="85%"
+		<a href="javascript:alert('Nom nom nom!');"> <img style="width: 85%; cursor: url(); position:relative; bottom: -60px;" src="<?php bloginfo('stylesheet_directory');?>/img/shark.png" width="85%"
 			class="aligncenter size-full wp-image-2723" />
 		</a>
 	</div>
