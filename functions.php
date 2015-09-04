9a51fa9233782f27b5c1c1fc975746b1d8d2da93
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2015-09-04 10:44:56 -0400

    Update amazon functions for new ad platform

--- functions.php
+++ functions.php
@@ -236,7 +236,7 @@ function amazon_func($atts) {
 	$books_html = "<div class='books'>";
 	foreach($exploded as $code)
 	{
-		$books_html .= "<iframe src='http://rcm.amazon.com/e/cm?lt1=_blank&bc1=$bgcolor&IS2=1&npa=1&bg1=$bgcolor&fc1=000000&lc1=0000FF&t=$referrer&o=1&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=".$code."' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='0'></iframe>";
+		$books_html .= "<iframe src='http://rcm-na.amazon-adsystem.com/e/cm?lt1=_blank&bc1=$bgcolor&IS2=1&npa=1&bg1=$bgcolor&fc1=000000&lc1=0000FF&t=$referrer&o=1&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=".$code."' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='0'></iframe>";
 	}
 	$books_html .= "</div>";
 	return $books_html;
